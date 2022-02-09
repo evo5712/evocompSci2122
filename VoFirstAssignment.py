df88e616818a4c4912297a8c089d84c9722faa96
evo5712 <99288143+evo5712@users.noreply.github.com>
2022-02-09 15:04:05 -0500

    Add files via upload

---
+++ VoFirstAssignment.py
@@ -0,0 +1,10 @@
+#VoAssignment1.py
+# Eric Vo
+# Feb. 9, 2022
+
+#create variable of type integer called side_a get user to "Enter side_a" into side_a
+side_a = int(input('Enter side_a'))
+
+#create variable of type integer surface_area and set it equal to 8.66 times side_a squared.
+surface_area = 8.66*side_a**2
+print(surface_area)
