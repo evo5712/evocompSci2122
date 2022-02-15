5e96de8f43d88026ff58505f46d94af050a70fff
evo5712 <99288143+evo5712@users.noreply.github.com>
2022-02-15 14:04:33 -0500

    Add files via upload

---
+++ MADLIBS.py
@@ -0,0 +1,26 @@
+noun1 = "apple"
+adjec1 = input('enter first adjective ')
+verb1 = input('enter verb with "ing" ')
+noun2 = input("enter second noun ")
+adverb1 = input('enter first adverb ')
+verb2 = input('enter verb with "ing" ')
+adjec2 = input('enter second adjective')
+adjec3 = input('enter third adjective')
+"""
+noun1 = input("enter first noun ")
+adjec1 = input('enter first adjective ')
+verb1 = input('enter verb with "ing" ')
+noun2 = input("enter second noun ")
+adverb1 = input('enter first adverb ')
+verb2 = input('enter verb with "ing" ')
+adjec2 = input('enter second adjective')
+adjec3 = input('enter third adjective')
+"""
+
+print(f"""I am exactly like a {noun1}.
+          I'm {adjec1}, just like it.
+          We're both capable of {verb1} others.
+          I'm the opposite of {noun2}.
+          It likes to {adverb1}, while I want to stay {verb2}.
+          It's seen as {adjec2}, while I'm {adjec3}.
+          """)
