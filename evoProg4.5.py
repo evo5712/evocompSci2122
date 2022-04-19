bdd57e10fc5c0e4a4ffad6e1b167c03a5d2a41b0
evo5712 <99288143+evo5712@users.noreply.github.com>
2022-04-19 14:29:38 -0400

    Add files via upload

---
+++ evoProg4.5.py
@@ -0,0 +1,24 @@
+def encrypt():
+    print("Type out what you would like to encrypt below.\n")
+    msg = input()
+    shift = int(input('\nWhat number would you like the shift to be? - '))
+    secret = ""
+    for c in msg:
+        if c.isupper() or c.islower():
+            c_index = ord(c) - ord("A")
+            new_index = (c_index + shift) % 26
+            new_unicode = new_index + ord("A")
+            new_character = chr(new_unicode)
+            secret = secret + new_character
+        else:
+            secret += c
+    print(f'\nYour encrypted message is: {secret}')
+    
+encrypt()
+            
+
+        
+        
+        
+        
+
