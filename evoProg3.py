20b456b8ba76de938638d15a3271cbbd2e0e8f8b
evo5712 <99288143+evo5712@users.noreply.github.com>
2022-04-25 14:08:12 -0400

    Update evoProg3.py

--- evoProg3.py
+++ evoProg3.py
@@ -1,12 +1,12 @@
-import cmath
-    
 a = float(input('\nWhat is a? - '))
 b = float(input('\nWhat is b? - '))
 c = float(input('\nWhat is c? - '))
-    
 d = (b**2) - (4*a*c)
-    
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
-    
-print('\nThe solution are {0} and {1}'.format(sol1,sol2)) 
+
+sqroot = (d ** 0.5)
+
+sol1 = (-b-sqroot)/(2*a)
+sol2 = (-b+sqroot)/(2*a)
+
+print('\nThe solution are {0} and {1}'.format(sol1,sol2))
+
