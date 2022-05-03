538593f0e10d90a945f2f4421353c8a334113bdc
evo5712 <99288143+evo5712@users.noreply.github.com>
2022-05-03 14:35:57 -0400

    Add files via upload

--- evoProg4.py
+++ evoProg4.py
@@ -1,24 +1,22 @@
-def encrypt():
-    print("Type out what you would like to encrypt below.\n")
-    msg = input()
-    shift = int(input('\nWhat number would you like the shift to be? - '))
-    secret = ""
-    for c in msg:
-        if c.isupper() or c.islower():
-            c_index = ord(c) - ord("A")
-            new_index = (c_index + shift) % 26
-            new_unicode = new_index + ord("A")
-            new_character = chr(new_unicode)
-            secret = secret + new_character
-        else:
-            secret += c
-    print(f'\nYour encrypted message is: {secret}')
+#Make a program that tells you how many occurences of one character there in comparison to total number of characters
+#have user select which letter they want to compare with
+#make a calculation of chosen letter / total characters
+#return percentage of how many characters ARE that chosen character
+def occurence(a):
+    f = open(a, 'r')
     
-encrypt()
-            
+    #Counting all characters, including punctuation + spaces
+    data = f.read().lower().replace(" ", "").replace(".", "").replace("?", "").replace("!", "")
+    char_count = len(data)
+    print(f'The total number of letters in your file is {char_count}.')
+    
+    #Getting the count for chosen letter
+    answer = str(input('\nFor which letter do you want to know how often it appears? Use lowercase only: '))
+    numerator = int(data.count(answer))
+    decimal = numerator / int(char_count)
+    percent = "{:.2f}".format(decimal * 100)
+    print(f"""\nThe letter "{answer}" appears this many times: {numerator}
+\nThat means roughly {percent}% of the letters in the text consists of {answer}'s.""")
 
-        
-        
-        
-        
-
+occurence('letterMAN.txt')
+    
