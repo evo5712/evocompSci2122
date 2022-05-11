a3668e4feabe6c879792e5ff7623a006aa990413
evo5712 <99288143+evo5712@users.noreply.github.com>
2022-05-11 14:45:14 -0400

    Add files via upload

---
+++ evoProg6.py
@@ -0,0 +1,24 @@
+#I'm trying to get the program to decrypt VW SFWQ as, "hi eric"
+def encrypt(a):
+    f = open(a, 'r')
+    secret = ""
+    for line in f:
+        string = line
+    for shift in range(26):
+        for c in string:
+            if c.isupper() or c.islower():
+                c_index = ord(c) - ord("A")
+                new_index = (c_index + shift) % 26
+                new_unicode = new_index + ord("A")
+                new_character = chr(new_unicode)
+                secret = secret + new_character
+            else:
+                secret += c
+        print(f'\nWe revealed the encrpytion to be {secret}.', end = '')
+        secret = ''
+        
+        
+encrypt('proj6 text.txt')
+
+
+
